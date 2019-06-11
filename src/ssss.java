41633b9e19838c3f78b98b6652b0f406f6479bc0
zhangsan <zhangsan@itcast.com>
2019-06-11 21:07:03 +0800

    实验中

---
+++ src/ssss.java
@@ -0,0 +1,10 @@
+public class ssss {
+    public static void main(String[] args) {
+        System.out.println("ddff");
+        System.out.println("ddff");
+        System.out.println("ddff");
+        System.out.println("ddff");
+        System.out.println("ddff");
+        System.out.println("ddff");
+    }
+}
